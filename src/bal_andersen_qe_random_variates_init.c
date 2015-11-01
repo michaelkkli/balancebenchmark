9e7e595e8bc168dbc39e42ac9204a69bd2c36a42
Michael Li <mikeliuk+bal@gmail.com>
2015-11-01 19:32:35 +0000

    Update using indent.

--- src/bal_andersen_qe_random_variates_init.c
+++ src/bal_andersen_qe_random_variates_init.c
@@ -5,41 +5,45 @@
 _bal_andersen_qe_random_variates
 _bal_andersen_qe_random_variates_init (_bal_andersen_qe_param param)
 {
-  size_t m = param->num_path_blocks * param->path_block_size * param->num_steps;
-  uint64_t * buf;
+  size_t m =
+    param->num_path_blocks * param->path_block_size * param->num_steps;
+  uint64_t *buf;
 
-  _bal_andersen_qe_random_variates rv = (typeof(rv))malloc(sizeof (_bal_andersen_qe_random_variates_internal)); if (!rv) return;
+  _bal_andersen_qe_random_variates rv =
+    (typeof (rv)) malloc (sizeof (_bal_andersen_qe_random_variates_internal));
+  if (!rv)
+    return;
 
-  rv->rand = (typeof(rv->rand))malloc(sizeof(double) * m * 3);
+  rv->rand = (typeof (rv->rand)) malloc (sizeof (double) * 3 * m);
   if (!rv->rand)
     {
-      free(rv);
+      free (rv);
       rv = 0;
       return;
     }
   rv->Zv = &rv->rand[0];
   rv->Uv = &rv->rand[m];
-  rv->Z = &rv->rand[2*m];
+  rv->Z = &rv->rand[2 * m];
 
-  buf = (uint64_t*)malloc (sizeof(uint64_t)* 3 * m);
-  if ( !buf )
+  buf = (uint64_t *) malloc (sizeof (uint64_t) * 3 * m);
+  if (!buf)
     {
-      _bal_andersen_qe_random_variates_destroy(rv);
+      _bal_andersen_qe_random_variates_destroy (rv);
       return;
     }
 
-  _bal_mrg8 (buf, 3*m);
-  _bal_mrg8_normalize (rv->rand, buf, 3*m);
+  _bal_mrg8 (buf, 3 * m);
+  _bal_mrg8_normalize (rv->rand, buf, 3 * m);
   _bal_pja_invnorm (rv->rand, m);
-  _bal_pja_invnorm (&rv->rand[2*m], m);
+  _bal_pja_invnorm (&rv->rand[2 * m], m);
 
-  free(buf);
-  
+  free (buf);
+
   return rv;
 }
 
 void
-_bal_andersen_qe_random_variates_destroy( _bal_andersen_qe_random_variates rv )
+_bal_andersen_qe_random_variates_destroy (_bal_andersen_qe_random_variates rv)
 {
   rv->Zv = 0;
   rv->Uv = 0;
